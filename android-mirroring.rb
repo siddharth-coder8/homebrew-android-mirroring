5ed86e9ce60aaa6cbbb14fb646add81d50afe0e1
SVECTOR <siddharth.svector@gmail.com>
2024-10-08 23:04:55 +0530

    Add Android Mirroring formula

---
+++ android-mirroring.rb
@@ -0,0 +1,99 @@
+class AndroidMirroring < Formula
+  desc "Display and control your Android device"
+  homepage "https://github.com/siddharth-coder8/Android-Mirroring.git"
+  url "https://github.com/siddharth-coder8/Android-Mirroring/archive/refs/tags/Updates.tar.gz"
+  sha256 "3ceea215f6eccb59535f68a16db6db2b05a8a1c91bdcb4a6e222d3093a9daf8c"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 arm64_sequoia: "f0fee1075318ee78ec35066cff51f5faef4d3af0c81ecdad25686f89394f3aa1"
+    sha256 arm64_sonoma:  "afaf0bf5fc3c48997ca8a36c626e40c673564c638ac951c44d667fc8782e1906"
+    sha256 arm64_ventura: "9ace7a93c8b40a371863463945b703f68424d78433803028bcd9a3b77f619920"
+    sha256 sonoma:        "f3b446582e45ca9972c8de85734ba16d293916479ae73e4f11a2884ef554df50"
+    sha256 ventura:       "84791d738ac1d933573c9ab27f773ab5b378257b4465122f8f1413caaa9d7545"
+    sha256 x86_64_linux:  "694bc1df5e0064f05df84169b5e07f7bbd1f54191d8d35892fc61eb771676d8e"
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "ffmpeg"
+  depends_on "libusb"
+  depends_on "sdl2"
+
+  fails_with gcc: "5"
+
+  resource "prebuilt-server" do
+    url "https://github.com/siddharth-coder8/Android-Mirroring/archive/refs/tags/Updates.tar.gz", using: :nounzip
+    sha256 "a23c5659f36c260f105c022d27bcb3eafffa26070e7baa9eda66d01377a1adba"
+  end
+
+  def install
+    odie "prebuilt-server resource needs to be updated" if version != resource("prebuilt-server").version
+
+    buildpath.install resource("prebuilt-server")
+    cp "scrcpy-server-v#{version}", "prebuilt-server.jar"
+
+    system "meson", "setup", "build", "-Dprebuilt_server=#{buildpath}/prebuilt-server.jar",
+                                      *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  def caveats
+    <<~EOS
+      At runtime, adb must be accessible from your PATH.
+
+      You can install adb from Homebrew Cask:
+        brew install --cask android-platform-tools
+    EOS
+  end
+
+  test do
+    fakeadb = (testpath/"fakeadb.sh")
+
+    # When running, scrcpy calls adb five times:
+    #  - adb start-server
+    #  - adb devices -l
+    #  - adb -s SERIAL push ... (to push scrcpy-server.jar)
+    #  - adb -s SERIAL reverse ... tcp:PORT ...
+    #  - adb -s SERIAL shell ...
+    # However, exiting on $3 = shell didn't work properly, so instead
+    # fakeadb exits on $3 = reverse
+
+    fakeadb.write <<~EOS
+      #!/bin/sh
+      echo "$@" >> #{testpath/"fakeadb.log"}
+
+      if [ "$1" = "devices" ]; then
+        echo "List of devices attached"
+        echo "emulator-1337          device product:sdk_gphone64_x86_64 model:sdk_gphone64_x86_64 device:emulator64_x86_64_arm64 transport_id:1"
+      fi
+
+      if [ "$3" = "reverse" ]; then
+        exit 42
+      fi
+    EOS
+
+    fakeadb.chmod 0755
+    ENV["ADB"] = fakeadb
+
+    # It's expected to fail after adb reverse step because fakeadb exits
+    # with code 42
+    out = shell_output("#{bin}/scrcpy --no-display --record=file.mp4 -p 1337 2>&1", 1)
+    assert_match(/ 42/, out)
+
+    log_content = File.read(testpath/"fakeadb.log")
+
+    # Check that it used port we've specified
+    assert_match(/tcp:1337/, log_content)
+
+    # Check that it tried to push something from its prefix
+    assert_match(/push #{prefix}/, log_content)
+  end
+end
